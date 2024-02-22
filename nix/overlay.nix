5992746c7ef0ede30b88eb2f1794f7b87240434f
Clo91eaf <Clo91eaf@qq.com>
2024-02-23 01:33:21 +0800

    Add elfloader example and nix files

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -3,5 +3,5 @@ final: prev:
 {
   libspike = final.callPackage ./pkgs/libspike.nix { };
   libspike-interfaces = final.callPackage ./pkgs/libspike-interfaces.nix { };
-  elfloader = final.callPackage ./tests/elfloader.nix { };
+  elfloader = final.callPackage ./examples/elfloader.nix { };
 }
